2071dbcbf6b118e2123e4413339fd7f53fa7e4f4
Jonathan Cheng <jcheng461@gmail.com>
2022-07-17 15:30:34 -0700

    set up async and await for api-service.js. WIP: display results from api call to user

--- src/index.js
+++ src/index.js
@@ -2,8 +2,34 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import apiService from "./api-service.js";
 
 // User Interface Logic
 $(document).ready(function () {
-  // Add here
+  $("#search").click(function () {
+    const ids = $("#searchIds").val();
+    clearFields();
+    apiCall(ids);
+  });
 });
+
+function clearFields() {
+  $("#searchIds").val("");
+  $(".showResults").empty();
+}
+
+async function apiCall(ids) {
+  const response = await apiService.getResults(ids);
+  getElements(response);
+}
+
+function getElements(response) {
+  if (response) {
+    for (let i = 0; i < 1; i++) {
+      // Show to user in html
+    }
+    $(".showResults").text(``);
+  } else {
+    $(".showErrors").text(`There was an error: ${response}`);
+  }
+}
